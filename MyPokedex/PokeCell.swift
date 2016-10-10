f8db0cb0e92913e141189a969abb561acb8e8d74
Dorio Hermann <hermann.dorio@epitech.eu>
2016-10-11 01:02:43 +0200

    add csv file properly implement pokemon models

---
+++ MyPokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  MyPokedex
+//
+//  Created by Hermann Dorio on 11/10/2016.
+//  Copyright © 2016 Hermann Dorio. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
